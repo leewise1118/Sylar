5d9d5b69299ef3a6c162134ca3d43354b0245c38
lee <1076774025@qq.com>
2023-05-31 20:55:00 +0800

    0531

--- sylar/iomanager.cpp
+++ sylar/iomanager.cpp
@@ -29,6 +29,7 @@ void IOManager::FdContext::resetContext( EventContext &ctx ) {
     ctx.fiber.reset();
     ctx.cb = nullptr;
 }
+// 触发事件上下文
 void IOManager::FdContext::triggerEvent( Event event ) {
     SYLAR_ASSERT( events & event );
 
@@ -42,22 +43,29 @@ void IOManager::FdContext::triggerEvent( Event event ) {
     ctx.scheduler = nullptr;
     return;
 }
+
 IOManager::IOManager( size_t thread, bool use_caller, const std::string &name )
     : Scheduler( thread, use_caller, name ) {
+
     m_epfd = epoll_create( 5000 ); // 创建epoll句柄，事件数目5000
     SYLAR_ASSERT( m_epfd > 0 );
 
     int rt = pipe( m_tickleFds );
-    SYLAR_ASSERT( !rt );
+    SYLAR_ASSERT( rt == 0 );
 
     epoll_event event;
     memset( &event, 0, sizeof( epoll_event ) );
-    event.events  = EPOLLIN | EPOLLET;
+    // 设置事件为读事件，并设置为水平触发模式(LT,level-triggered)
+    event.events = EPOLLIN | EPOLLET;
+    // 设置事件句柄位于管道读端
     event.data.fd = m_tickleFds[ 0 ];
 
+    // fcntl,file control 根据文件描述词来操作文件的特性
+    // 将读端设置为非阻塞
     rt = fcntl( m_tickleFds[ 0 ], F_SETFL, O_NONBLOCK );
     SYLAR_ASSERT( !rt );
 
+    // 将监听的文件描述符添加到epoll实例中
     rt = epoll_ctl( m_epfd, EPOLL_CTL_ADD, m_tickleFds[ 0 ], &event );
     SYLAR_ASSERT( !rt );
 
@@ -86,8 +94,9 @@ void IOManager::contextResize( size_t size ) {
     }
 }
 int IOManager::addEvent( int fd, Event event, Func cb ) {
-    FdContext          *fd_ctx = nullptr;
+    FdContext          *fd_ctx = nullptr; // 创建事件上下文
     MutexType::ReadLock Rlock( m_mutex );
+    // 如果在事件池里就取出来，如果不在就resize事件池,再取出来
     if ( (int) m_fdContexts.size() > fd ) {
         fd_ctx = m_fdContexts[ fd ];
         Rlock.unlock();
@@ -97,6 +106,7 @@ int IOManager::addEvent( int fd, Event event, Func cb ) {
         contextResize( fd * 1.5 );
         fd_ctx = m_fdContexts[ fd ];
     }
+
     FdContext::MutexType::Lock lock( fd_ctx->mutex );
     if ( SYLAR_UNLIKELY( fd_ctx->events & event ) ) {
         SYLAR_LOG_ERROR( g_logger )
@@ -104,12 +114,17 @@ int IOManager::addEvent( int fd, Event event, Func cb ) {
             << " fd_ctx.event =" << fd_ctx->events;
         SYLAR_ASSERT( !( fd_ctx->events & event ) );
     }
-    int         op = fd_ctx->events ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;
+    // 事件存在则更改文件描述相关联的事件，事件不存在就在epoll实例上注册目标事件。
+    int op = fd_ctx->events ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;
+
     epoll_event epevent;
+    // 边沿触发(ET,edge-trigger)
     epevent.events   = EPOLLET | fd_ctx->events | event;
     epevent.data.ptr = fd_ctx;
 
+    // 更改或注册事件, 并关联到fd上
     int rt = epoll_ctl( m_epfd, op, fd, &epevent );
+
     if ( rt ) {
         SYLAR_LOG_ERROR( g_logger )
             << "epoll_ctl(" << m_epfd << ", " << op << "," << fd << ","
@@ -118,11 +133,13 @@ int IOManager::addEvent( int fd, Event event, Func cb ) {
         return -1;
     }
 
-    ++m_pendingEventCount;
+    ++m_pendingEventCount; // 等待执行的事件数量+1
     fd_ctx->events = (Event) ( fd_ctx->events | event );
 
     FdContext::EventContext &event_ctx = fd_ctx->getContext( event );
+
     SYLAR_ASSERT( !event_ctx.scheduler && !event_ctx.fiber && !event_ctx.cb );
+    // 更改事件上下文内容
     event_ctx.scheduler = Scheduler::GetThis();
     if ( cb ) {
         event_ctx.cb.swap( cb );
@@ -133,6 +150,7 @@ int IOManager::addEvent( int fd, Event event, Func cb ) {
     }
     return 0;
 }
+
 bool IOManager::delEvent( int fd, Event event ) {
     MutexType::ReadLock Rlock( m_mutex );
     if ( (int) m_fdContexts.size() <= fd ) {
@@ -232,6 +250,7 @@ bool IOManager::cancelAll( int fd ) {
     return true;
 }
 IOManager *IOManager::GetThis() {
+    // 基类向子类转换
     return dynamic_cast<IOManager *>( Scheduler::GetThis() );
 }
 
@@ -239,6 +258,7 @@ void IOManager::tickle() {
     if ( !hasIdleThread() ) {
         return;
     }
+    // 给写端写数据，写入"T"
     int rt = write( m_tickleFds[ 1 ], "T", 1 );
     SYLAR_ASSERT( rt == 1 );
 }
@@ -253,7 +273,9 @@ bool IOManager::stopping( uint64_t &timeout ) {
 }
 void IOManager::idle() {
     SYLAR_LOG_DEBUG( g_logger ) << "idle";
-    epoll_event                 *events = new epoll_event[ 64 ]();
+    epoll_event *events = new epoll_event[ 64 ]();
+
+    // 智能指针托管怠惰事件
     std::shared_ptr<epoll_event> shared_events(
         events, []( epoll_event *ptr ) { delete[] ptr; } );
     while ( true ) {
@@ -273,7 +295,7 @@ void IOManager::idle() {
             } else {
                 next_timeout = MAX_TIMEOUT;
             }
-
+            // epoll_wait等待epoll事件从epoll实例中发生，并返回对应的描述符
             rt = epoll_wait(
                 m_epfd, events, 64,
                 (int) next_timeout ); // 查看事件是否触发,返回事件的长度
@@ -283,6 +305,7 @@ void IOManager::idle() {
                 break;
             }
         } while ( true );
+
         std::vector<Func> cbs;
         listExpiredCb( cbs );
         if ( !cbs.empty() ) {
@@ -299,7 +322,7 @@ void IOManager::idle() {
                 continue;
             }
 
-            FdContext *fd_ctx = (FdContext *) event.data.ptr; // 将
+            FdContext *fd_ctx = (FdContext *) event.data.ptr; //
 
             FdContext::MutexType::Lock lock( fd_ctx->mutex );
             if ( event.events & ( EPOLLERR | EPOLLHUP ) ) {
--- sylar/iomanager.h
+++ sylar/iomanager.h
@@ -11,39 +11,81 @@ class IOManager : public Scheduler, public TimerManager {
     using Func      = std::function<void()>;
 
     enum Event {
-        NONE  = 0x0,
-        READ  = 0x1, // EPOLLIN
-        WRITE = 0x4, // EPOLLOUR
+        NONE  = 0x0, // 无事件
+        READ  = 0x1, // 读事件EPOLLIN
+        WRITE = 0x4, // 写事件EPOLLOUT
     };
 
   private:
+    /**
+     * @brief Socket事件上下文类
+     */
     struct FdContext {
         using MutexType = Mutex;
+
+        /**
+         * @brief 事件上下文类
+         */
         struct EventContext {
             Scheduler            *scheduler; // 事件执行的scheduler
             Fiber::ptr            fiber;     // 事件协程
             std::function<void()> cb;        // 事件的回调函数
         };
+
+        /**
+         * @brief 获取事件上下文类
+         * @param event 事件类型
+         * @return 返回对应的时间上下文
+         */
         EventContext &getContext( Event event );
-        void          resetContext( EventContext &ctx );
-        void          triggerEvent( Event event );
+
+        /**
+         * @brief 重置事件上下文
+         * @param ctx:待重置的事件上下文类
+         */
+        void resetContext( EventContext &ctx );
+
+        /**
+         * @brief 触发事件
+         * @param event:事件类型
+         */
+        void triggerEvent( Event event );
 
         int          fd = 0;        // 事件关联的句柄
-        EventContext read;          // 读事件
-        EventContext write;         // 写事件
-        Event        events = NONE; // 已经注册的事件
+        EventContext read;          // 读事件上下文
+        EventContext write;         // 写事件上下文
+        Event        events = NONE; // 当前的事件
         MutexType    mutex;
     };
 
   public:
+    /**
+     * @brief Construct a new IOManager object
+     *
+     * @param thread: 线程数量
+     * @param use_caller: 是否将调用线程包含进去
+     * @param name 调度器的名称
+     */
     IOManager( size_t thread = 1, bool use_caller = true,
                const std::string &name = "" );
     ~IOManager();
-    // 0 success, -1 error
-    int               addEvent( int fd, Event event, Func cb = nullptr );
-    bool              delEvent( int fd, Event event );
-    bool              cancelEvent( int fd, Event event );
-    bool              cancelAll( int fd );
+
+    /**
+     * @brief 添加事件
+     * @param fd socket句柄
+     * @param event 事件类型
+     * @param cb 事件回调函数
+     * @return int 添加成功返回0，失败返回-1
+     */
+    int addEvent( int fd, Event event, Func cb = nullptr );
+
+    bool delEvent( int fd, Event event );
+    bool cancelEvent( int fd, Event event );
+    bool cancelAll( int fd );
+
+    /**
+     * @brief 返回当前的IOManager
+     */
     static IOManager *GetThis();
 
   protected:
@@ -53,14 +95,18 @@ class IOManager : public Scheduler, public TimerManager {
     void idle() override;
     void onTimeInsertedAtFront() override;
 
+    /**
+     * @brief 重置socket句柄上下文的容器大小
+     * @param size 容器大小
+     */
     void contextResize( size_t size );
 
   private:
-    int                      m_epfd = 0;
-    int                      m_tickleFds[ 2 ];
-    std::atomic<size_t>      m_pendingEventCount = { 0 };
+    int m_epfd = 0;                                  // epoll文件句柄
+    int m_tickleFds[ 2 ];                            // pipe文件句柄
+    std::atomic<size_t> m_pendingEventCount = { 0 }; // 当前等待执行的事件数量
     MutexType                m_mutex;
-    std::vector<FdContext *> m_fdContexts;
+    std::vector<FdContext *> m_fdContexts; // socket事件上下文容器
 };
 
 } // namespace sylar
